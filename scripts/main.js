b1704bcd664a2f90cca0c390a4981f574089b7ee
madguy <madguy@drumcan.in>
2022-04-13 04:59:16 +0900

    デフォルトの関数を更新

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,49 @@
 'use strict';
-const BUTTONS = document.querySelectorAll("#keys > button");
+const BUTTONS = document.querySelectorAll('#keys > button');
 
-function writeResult(text = '0') {
-  document.querySelector('#result').innerText = text
+function writeResult(text) {
+  document.querySelector('#result').innerText = text || '0';
 }
 
+function addText(value) {}
+
+function saveOperator(value) {}
+
+function clear() {}
+
+function clearEntry() {}
+
 for (let button of BUTTONS) {
-  button.addEventListener("click", (e) => {
+  button.addEventListener('click', (e) => {
     let element = e.target;
-    console.log(element.dataset.value);
+    let value = element.dataset.value;
+    switch (value) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        addText(value);
+        break;
+      case 'plus':
+      case 'minus':
+      case 'multiply':
+      case 'divide':
+        saveOperator(value);
+        break;
+      case 'equal':
+        break;
+      case 'clear':
+        clear();
+        break;
+      case 'ce':
+        clearEntry();
+        break;
+    }
   });
 }
